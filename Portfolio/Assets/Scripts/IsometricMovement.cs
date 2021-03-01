037f637171a7f65ed78595826b7e239eea741acb
AndreMejiaGonzalez <andremejia64@gmail.com>
2021-02-28 21:18:01 -0800

    implemented player movement roll

--- Portfolio/Assets/Scripts/IsometricMovement.cs
+++ Portfolio/Assets/Scripts/IsometricMovement.cs
@@ -7,9 +7,13 @@ public class IsometricMovement : MonoBehaviour
     
     [SerializeField]
     private float moveSpeed;
+    [SerializeField]
+    private float rotateRate;
 
     private Rigidbody rb;
     private Vector3 forward, right;
+    private float heading;
+    private float rotateTimer;
 
     void Start()
     {
@@ -18,27 +22,76 @@ public class IsometricMovement : MonoBehaviour
         forward.y = 0f;
         forward = Vector3.Normalize(forward);
         right = Quaternion.Euler(new Vector3(0,90,0)) * forward;
+        rotateTimer = rotateRate;
     }
 
     void Update()
     {
-        if(Input.anyKey)
+        if(Input.GetAxisRaw("Horizontal") != 0 || Input.GetAxisRaw("Vertical") != 0)
         {
             movePlayer();
+            rotateTimer += Time.deltaTime;
+            if(rotateTimer >= rotateRate)
+            {
+                rollForward();
+                rotateTimer = 0;
+            }
         } else {
             rb.velocity = Vector3.zero;
+            rotateTimer = rotateRate;
+        }
+
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            rollForward();
         }
     }
 
     void movePlayer()
     {
+
         Vector3 rightMovement = right * moveSpeed * Input.GetAxisRaw("Horizontal");
         Vector3 upMovement = forward * moveSpeed * Input.GetAxisRaw("Vertical");
 
-        Vector3 heading = Vector3.Normalize(rightMovement + upMovement);
+        Vector3 direction = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
 
-        transform.forward = heading;
+        switch (direction.x, direction.y)
+        {
+            case (1, 0):
+                heading = 135;
+                break;
+            case (-1,0):
+                heading = -45;
+                break;
+            case (0,1):
+                heading = 45;
+                break;
+            case (0,-1):
+                heading = -135;
+                break;
+            case (1,1):
+                heading = 90;
+                break;
+            case (1,-1):
+                heading = -180;
+                break;
+            case (-1,1):
+                heading = 0;
+                break;
+            case (-1,-1):
+                heading = -90;
+                break;
+            default:
+                break;
+        }
+
+        transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles.x, heading, transform.rotation.eulerAngles.z);
 
         rb.velocity = (rightMovement + upMovement);
+    }
+
+    void rollForward()
+    {
+        transform.Rotate(new Vector3(90,0,0), Space.Self);
     }
 }
